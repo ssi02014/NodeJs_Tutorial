9ad8090f23325af78e2e508cbc4024ec99cec660
JeonMinJae <ssi02014@naver.com>
2021-12-03 01:05:48 +0900

    Add(minjae): multer

--- ch6/app.js
+++ ch6/app.js
@@ -4,10 +4,13 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const dotenv = require("dotenv");
+const multer = require("multer");
+const fs = require("fs");
 
 dotenv.config();
 
 const app = express();
+
 app.set("port", process.env.PORT || 8000);
 
 app.use(morgan("dev"));
@@ -26,6 +29,39 @@ app.use(
     },
     name: "session-cookie",
   })
+);
+
+try {
+  fs.readdirSync("uploads");
+} catch (err) {
+  console.log("uploads 폴더가 없어 uploads 폴더를 생성한다.");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+
+app.post(
+  "/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send("ok");
+  }
 );
 
 app.use((req, res, next) => {
