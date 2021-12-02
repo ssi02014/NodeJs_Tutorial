0ac973778f5bc0f8d5a11bb138d17e8c19eb2d68
JeonMinJae <ssi02014@naver.com>
2021-12-03 00:13:03 +0900

    Add(minjae): middleware

--- ch6/app.js
+++ ch6/app.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 app.set("port", process.env.PORT || 8000);
+
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
 
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행");
